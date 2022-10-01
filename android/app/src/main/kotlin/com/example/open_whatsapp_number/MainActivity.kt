18dd8f57b022ca4f6f9c917ffdff2004c72035d0
Leonardo Gonzalez dos Santos <59115521+leozero3@users.noreply.github.com>
2022-10-01 12:04:28 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/open_whatsapp_number/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.open_whatsapp_number
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
